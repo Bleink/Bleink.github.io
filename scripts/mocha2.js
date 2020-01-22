64252ba443f017ff9bd8c8ce67fb9c4038474d6a
Bleink <57928442+Bleink@users.noreply.github.com>
2020-01-22 12:38:46 -0600

    Week Two Day Two

---
+++ scripts/mocha2.js
@@ -0,0 +1,172 @@
+"use strict";
+
+/*
+	This function compares two inputs and returns the larges one
+*/
+function maxTwo(a,b){
+    if(a>b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+/*
+	This function compares three inputs and returns the larges one
+*/
+function maxThree(a,b,c){
+    let max=0;
+
+    if(a>b){
+        if(a>c) {
+            return a;
+        }
+        else if(b>c){
+            {
+                return b;
+            }
+        }
+        else if(b>c){
+            return b
+        }
+
+        else {
+            return c;
+        }
+    }
+}
+
+
+/*
+	This function checks if an input is a vowel
+*/
+function isVowel(ch){
+    if(ch === 'a' || ch === 'e' || ch === 'i' || ch ==='o' || ch === 'u' )
+        return true ;
+    else
+        return false;
+}
+
+/*
+	This function sums the elements of an input array
+*/
+function sum(arr){
+    let sum = 0;
+    for( const i of arr){
+        sum+=i;
+    }
+
+    return sum;
+}
+
+/*
+	This function multiply the elements of an input array
+*/
+function multiply(arr){
+    let mul = 1;
+    for( const i of arr){
+        mul*=i;
+    }
+
+    return mul;
+}
+
+/*
+	This function reverses a string input
+*/
+function stringReverse(input){
+    let reversedInput = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        reversedInput += input[i];
+    }
+
+    return reversedInput;
+}
+
+
+/*
+	This function returns the longes word from an array of word inputs
+*/
+function findLongestWord(arr){
+    let longest = "";
+    for( let i of arr){
+        if(i.length>longest.length)
+            longest = i;
+    }
+    return longest;
+}
+
+/*
+	This function returns the list of longes words from an array of word inputs lengths greater than  a given length
+*/
+function filterLongWords (arr,num){
+    let longestList = [];
+    for( let i of arr){
+        if(num<i.length)
+            longestList.push(i);
+    }
+
+    return longestList;
+}
+
+/*
+	This function multiplies each element of an array by 10
+*/
+
+
+function multiplyByTen(){
+    const c = [1,2,3];
+    const e = c.map(function(elem, i, array) {
+        return elem *10;
+    })
+    return e;
+}
+
+/*
+	This function selects each element of an array with value of 3
+*/
+function elementOf3(){
+    const a = [1,3,5,3,3];
+
+    const c = a.filter(function(elem, i, array){
+        return elem === 3;
+    });
+    return c;
+}
+
+/*
+	This function multiplies each element of an array each other
+*/
+function arrayProduct(){
+    const a = [1,2,3,4];
+
+    const d = a.reduce(function(prevValue, elem, i, array){
+        return prevValue * elem;
+    });
+    return d;
+}
+
+
+//=========================================================================
+
+function myMap(arr, mapFunc) {
+
+    const mapArr = [];
+    for(let i=0;i<arr.length;i++) {
+        const result = mapFunc(arr[i], i, arr);
+        mapArr.push(result);
+    }
+    return mapArr;
+}
+
+function myFilter(arr, filterFunc) {
+    const filterArr = [];
+    for(let i=0;i<arr.length;i++) {
+        const result = filterFunc(arr[i], i, arr);
+        if(result)
+            filterArr.push(arr[i]);
+    }
+    return filterArr;
+}
+
+
---
+++ scripts/reverselist.js
@@ -0,0 +1,159 @@
+//========================== REVERSE and REVERSE IN PLACE============
+
+//(function(){
+"use strict";
+//go through each array element starting from end
+//and copy it to a new array to be returned.
+function reverseArray(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray[i] = arr[arr.length - i - 1];
+    }
+    return newArray;
+}
+//traverse the array forward while saving each value in a temporary
+//variable and swapping it to its symmetric location after half length
+function reverseArrayInPlace(arr) {
+    for (let i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = temp;
+    }
+    return arr;
+}
+
+console.log(reverseArray([ "A", "B", "C" ]));
+let arrayValue = [ 1, 2, 3, 4, 5 ];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+"use strict";
+function arrayToList(arr) {
+    let retVal = {
+        rest : null
+
+    };
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (retVal.rest === null) {
+            retVal.value = arr[i];
+            retVal.rest = {
+
+                rest : null
+            };
+        } else {
+            let counter = retVal;
+            while (counter.rest !== null) {
+                counter = counter.rest;
+            }
+            counter.value = arr[i];
+            counter.rest = {
+
+                rest : null
+            };
+
+        }
+
+    }
+
+    return retVal;
+
+}
+
+function listToArray(list) {
+
+    let arr = [];
+
+    while (list !== null) {
+        if (list.value !== undefined)
+            arr.push(list.value);
+        list = list.rest;
+    }
+
+    return arr;
+
+}
+
+
+function prepend(element, list) {
+    return {
+        value : element,
+        rest : list
+    };
+
+}
+
+
+function nth(list, index) {
+    return listToArray(list)[index];
+
+}
+
+function nthNextMethod(list, index) {
+    while (list !== null) {
+        if (list.value !== undefined) {
+
+            if (index-- === 0) {
+                return list.value;
+            }
+        }
+        list = list.rest;
+
+    }
+
+}
+
+function recursiveNth(list, index) {
+
+    if (index === 0)
+        return list.value;
+
+    return recursiveNth(list.rest, --index);
+
+}
+
+console.log(recursiveNth(arrayToList([ 10, 20, 30 ]), 1));
+console.log(nth(arrayToList([ 10, 20, 30 ]), 1));
+console.log(nthNextMethod(arrayToList([ 10, 20, 30 ]), 1));
+
+"use strict";
+
+/**
+ * Compares two objects for equal values in all properties and recursively for any objects that are property values
+ * @param a any object
+ * @param b another object to be compared
+ * @returns {boolean} true if they are deep equal, else false
+ */
+function deepEqual(a, b) {
+    /* check to see if equal primitive values or same object reference */
+    if (a === b) {return true; }
+
+    /* must both be objects or else cannot have equal values (after first test)*/
+    if (a === null || typeof a !== "object" ||
+        b === null || typeof b !== "object") { return false;}
+
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+
+    /* must have same number of properties */
+    if (keysA.length !== keysB.length) {return false; }
+
+    /* must have all the same properties and the values of properties must be the same */
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {return false; }
+    }
+
+    return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+console.log(deepEqual(null, null));
+const x = {a:1, b:2};
+const y = x;
+console.log(deepEqual(x, y));
